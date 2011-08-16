25630f706406df31a545b7f342f57d4feb87c0fc
Scott Ballantyne <ussballantyne@gmail.com>
2011-08-16 18:21:00 +0800

    nothing to see here

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 dx = require('./index');
 util = require('util');
-dx.init('00001312', 'NzM2OTY3NzQwMDAzNzQ4OTMyOTY4MjE2');
-dx.request({number: '13917086730', message: 'testtext'});
+dx.init('apid', 'key');
+dx.request({number: 'phone-number', message: 'message you want to say'});
 
 
